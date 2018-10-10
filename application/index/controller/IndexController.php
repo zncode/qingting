36429f6841d0cab294245cfd79a91007e4530f4e
赵宁 <zhaoning@taoyibao.com>
2018-10-10 09:47:17 +0800

    列表页修改

--- application/index/controller/IndexController.php
+++ application/index/controller/IndexController.php
@@ -10,6 +10,11 @@ class IndexController extends BaseController
         return view('index/index');
     }
 
+    public function category_list()
+    {
+        return view('index/category_list');
+    }
+
     public function resource_list()
     {
 //        echo 'test';die;
--- application/route_index.php
+++ application/route_index.php
@@ -2,5 +2,6 @@
 use think\Route;
 
 //首页
-Route::get('index',      'index/IndexController/index');
+Route::get('index',         'index/IndexController/index');
+Route::get('category',      'index/IndexController/category_list');
 
