46dd3db1c40af0d2bf33a996ab994e924784e6d8
2005znwy@163.com <2005znwy@163.com>
2019-02-11 15:19:15 +0800

    修改排序asc

--- application/index/controller/IndexController.php
+++ application/index/controller/IndexController.php
@@ -116,7 +116,7 @@ class IndexController extends BaseController
                     ->join('upload b', 'a.thumb = b.id', 'left')
                     ->join('taxonomy c', 'a.taxonomy_id = c.id', 'left')
                     ->where(array('a.taxonomy_id'=>$child['id'],'a.delete'=>0))
-                    ->order('create_time desc')
+                    ->order('create_time asc')
                     ->limit(20)
                     ->select();
 
